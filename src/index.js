39acb1fd7249c3e0078ed06770806684e09881dc
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-03 23:34:39 -0500

    Added working functionality to follow a specified user

--- src/index.js
+++ src/index.js
@@ -65,5 +65,46 @@ request.get({
         jar
     }, (error, response, body) => {
         console.log(response.body);
+
+        let cookieStringsArray2 = jar.getCookieString('https://www.instagram.com/').split(' ')
+
+        let csrfTokenIndex2 = 0;
+        for (let i = 0; i < cookieStringsArray2.length; i++) {
+            if (cookieStringsArray2[i].includes('csrftoken')) {
+                csrfTokenIndex2 = i;
+            }    
+        }
+
+        // As long as the new csrf token is fetched, requests will continue to stay authenticated.
+        const csrfTokenValue2 = cookieStringsArray2[csrfTokenIndex2].split('=')[1].slice(0, -1);
+        console.log(jar);
+        console.log(csrfTokenValue2);
+
+        const HEADERS_FOLLOW= {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive',
+            'Content-Length': 0,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': process.env.INSTAGRAM_URI_BASE_WWW,
+            'Origin': process.env.INSTAGRAM_URI_BASE_HTTPS_WWW,
+            'Referer': 'https://www.instagram.com/ersunyanal/',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36',
+            'X-CSRFToken': csrfTokenValue2,
+            'X-IG-App-ID': '936619743392459',
+            'X-Instagram-AJAX': '6c66f7292327-hot',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+
+        request.post({
+            headers: HEADERS_FOLLOW,
+            url: 'https://www.instagram.com/web/friendships/1369572015/follow/', // didn't need gzip or redirect options for /follow
+            jar
+        }, (error, response, body) => {
+            console.log(body);
+        });
     });
 });
