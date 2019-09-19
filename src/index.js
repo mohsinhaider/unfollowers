de287d164b85b55181bdcf8a774650739178b7e9
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-18 22:35:16 -0500

    Added comments to routes and added try catch to login route

--- src/index.js
+++ src/index.js
@@ -9,13 +9,25 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
+/**
+ * Logs in the API server to Instagram
+ * @name GET/api/v1/login
+*/
 app.get('/login', async (req, res) => {
-    login((error, response) => {
-        // TODO: Add logic here
-    });
+    try {
+        login((error, response) => {
+            // TODO: Add logic here
+        });
+    }
+    catch (error) {
+
+    }
 });
 
-// Testing purpose only, works after /login is called.
+/**
+ * Retrieves the CSRF token generated at login time.
+ * @name GET/api/v1/token
+*/
 app.get('/token', (req, res) => {
     res.send(process.env.SERVER_CSRF_TOKEN_VALUE);
 });
