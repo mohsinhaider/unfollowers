026ca62da197cb6a6f4c9d46fda444d1b5c1c54a
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-19 00:04:07 -0500

    Added descriptive comments to login.js

--- src/requests/login.js
+++ src/requests/login.js
@@ -5,16 +5,22 @@ const { initialCsrfTokenHeaders, loginHeaders } = require('../headers/headers');
 
 module.exports = {
     login: async (callback) => {
+        // Explicitly create a request Cookie jar for reuse throughout the request module calls
         let jar = request.jar();
 
+        // Initial request to /web/__mid to get pre-login CSRF token
         request.get({
             headers: initialCsrfTokenHeaders,
             url: process.env.INSTAGRAM_URI_GET_CSRF_TOKEN,
             jar
         }, (error, response, body) => {
+            // Extract the cookie values that were populated in the request jar into an array
             const preLoginCookieStringsArray = jar.getCookieString(process.env.INSTAGRAM_URI_GET_CSRF_TOKEN).split(' ');
+            
+            // Retrieve the index that in the array of cookies that represents the CSRF token
             let preLoginCsrfTokenIndex = 0;
             
+            // TODO: Fix code duplication by introducing helper method
             for (let i = 0; i < preLoginCookieStringsArray.length; i++) {
                 if (preLoginCookieStringsArray[i].includes('csrftoken')) {
                     preLoginCsrfTokenIndex = i;
@@ -24,6 +30,7 @@ module.exports = {
             // Add CSRF token required for login request to http headers
             loginHeaders['X-CSRFToken'] = preLoginCookieStringsArray[preLoginCsrfTokenIndex].split('=')[1].slice(0, -1);
 
+            // Request to /accounts/login that gets login CSRF token value that is reusable across successive Instagram requests
             request.post({
                 headers: loginHeaders,
                 url: process.env.INSTAGRAM_URI_LOGIN,
@@ -39,6 +46,7 @@ module.exports = {
 
                 let loginCookieStringsArray = jar.getCookieString(process.env.INSTAGRAM_URI_BASE_HTTPS_WWW).split(' ')
 
+                // TODO: Fix code duplication by introducing helper method
                 let loginCsrfTokenIndex = 0;
                 for (let i = 0; i < loginCookieStringsArray.length; i++) {
                     if (loginCookieStringsArray[i].includes('csrftoken')) {
