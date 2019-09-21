c602837ae5b7e16027a0d5368fb8b8d500cae99a
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-21 00:00:52 -0500

    Commented express.static call

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Set public assets directory
 app.use(express.static(publicDirectoryPath));
 
 /**
