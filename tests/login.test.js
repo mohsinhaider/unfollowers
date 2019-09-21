6a9c1635e6b5e6b1deac9555836c9860149151f0
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-21 18:09:54 -0500

    Implemented passing status code test for /login

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,2 +1,8 @@
 const app = require('../src/app');
 const supertest = require('supertest');
+
+test('Should login an existing Instagram user', async () => {
+    const response = await supertest(app)
+        .get('/login')
+        .expect(200);
+});
