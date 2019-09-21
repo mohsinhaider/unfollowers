048fb2a5cb851c09d18e38f441327928669b7eec
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-21 18:36:05 -0500

    Logged in the server immediately during start

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const { login } = require('./requests/login');
 const path = require('path');
+
+const { login } = require('./requests/login');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -12,6 +13,14 @@ app.use(express.static(publicDirectoryPath));
 // Utilize body-parser as Express middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Log in the server to Instagram immediately
+login((error, response) => {
+    if (error) {
+        throw new Error('Server could not log into Instagram, exiting abruptly.')
+    }
+    console.log('Login was successful');
+});
 
 /**
  * Logs in the API server to Instagram
