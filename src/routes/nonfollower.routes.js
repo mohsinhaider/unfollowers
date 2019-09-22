dc58f973b40ffb6afdf087ce38d24dcdb89ce2d4
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-21 22:52:09 -0500

    Wrote docstring

--- src/routes/nonfollower.routes.js
+++ src/routes/nonfollower.routes.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+/**
+ * Calculates the set of your Instagram followers 
+ * that do not follow you back ('nonfollowers').
+ * @name GET/api/nonfollowers
+*/
 router.post('/', (req, res) => {
     res.sendStatus(200);
 });
