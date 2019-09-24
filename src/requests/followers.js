46c85410e6418832da749aa2dcd8eb4153777a39
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-23 22:52:13 -0500

    Added Cookie request header and populated it with sessionid key-value pair

--- src/requests/followers.js
+++ src/requests/followers.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 module.exports = {
-    followers: (username) => {
+    followers: (csrfToken, sessionId) => {
         const followersGraphqlQueryHash = 'c76146de99bb02f6415203be841dd25a';
         const followersVariables = {
             id: '19288260011',
@@ -11,21 +11,21 @@ module.exports = {
         }
 
         const followersRequestUrl = `https://www.instagram.com/graphql/query/?query_hash=${followersGraphqlQueryHash}&variables=${encodeURIComponent(JSON.stringify(followersVariables))}`;
+        const sessionIdCookieKeyValuePair = 'sessionid=' + sessionId + ';';
 
         request.get({
             headers: {
                 'Accept': '*/*',
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Accept-Language': 'en-US,en;q=0.9',
-                // Cookie below is for Debug, figure out how to get 'sessionid', probably just have to pass cookie jar here.
-                'Cookie': `rur=PRN; mid=XYgftwAEAAHE3ymbOB2HlsZOfchQ; shbid=1866; shbts=1569202116.8307796; csrftoken=92Zjf6ocrF5evMWaRtVvCMqJrOx2na4n; ds_user_id=19288260011; sessionid=19288260011%3APhjZnUqhAZcwPr%3A19;`,
+                'Cookie': sessionIdCookieKeyValuePair,
                 'Connection': 'keep-alive',
                 'Host': process.env.INSTAGRAM_URI_BASE_WWW,
                 'Referer': 'https://www.instagram.com/roxy.tillerson/followers/',
                 'Sec-Fetch-Mode': 'cors',
                 'Sec-Fetch-Site': 'same-origin',
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36',
-                'X-CSRFToken': username,
+                'X-CSRFToken': csrfToken,
                 'X-IG-App-ID': '936619743392459',
                 'X-Requested-With': 'XMLHttpRequest'
             },
