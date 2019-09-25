652787a73b4fb591c98088d9ebe356f7af976302
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-25 00:01:48 -0500

    TODO for error case when db cant connect

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const databaseUri = process.env.MONGODB_DEV_URI;
 
+// TODO: Add callback to handle error case
 mongoose.connect(databaseUri, {
     useNewUrlParser: true,
     useCreateIndex: true
