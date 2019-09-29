2008d611f6ada7a04ef2fa20063129df0a43020a
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-28 23:26:17 -0500

    Created user metadata query url

--- src/requests/userid.js
+++ src/requests/userid.js
@@ -3,5 +3,7 @@ const request = require('request');
 module.exports = {
     userid: (username) => {
         // TODO: validate username
+        const instagramUserMetadataUriQueryString = '?__a=1';
+        const instagramUserMetadataUri = process.env.INSTAGRAM_URI_BASE_HTTPS_WWW + '/' + username + '/' + instagramUserMetadataUriQueryString;
     }
 }
