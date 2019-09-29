04311f4f0e0e7e51d8bea4f29dc0905c72684dbc
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-29 00:18:26 -0500

    Implemented functionality to fetch user id

--- src/requests/userid.js
+++ src/requests/userid.js
@@ -2,8 +2,24 @@ const request = require('request');
 
 module.exports = {
     userid: (username) => {
-        // TODO: validate username
-        const instagramUserMetadataUriQueryString = '?__a=1';
-        const instagramUserMetadataUri = process.env.INSTAGRAM_URI_BASE_HTTPS_WWW + '/' + username + '/' + instagramUserMetadataUriQueryString;
+        return new Promise((resolve, reject) => {
+            const instagramUserMetadataUriQueryString = '?__a=1';
+            const instagramUserMetadataUri = process.env.INSTAGRAM_URI_BASE_HTTPS_WWW + '/' + username + '/' + instagramUserMetadataUriQueryString;
+
+            request.get({
+                url: instagramUserMetadataUri
+            }, (error, response) => {
+                if (response.statusCode === 404) {
+                    return reject('Instagram user does not exist.');
+                }
+
+                if (error || response.statusCode != 200) {
+                    return reject('Instagram user metadata endpoint could not respond with success.');
+                }
+
+                const responseObject = JSON.parse(response.body);
+                return resolve(responseObject.graphql.user.id);
+            })
+        }) ;
     }
 }
