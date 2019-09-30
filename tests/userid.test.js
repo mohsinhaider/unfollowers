447450dd72e6b1b10ddce08a8d8a7408bf9e89c4
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-29 21:49:36 -0500

    Wrote test for userid() functionality

---
+++ tests/userid.test.js
@@ -0,0 +1,13 @@
+/**
+ * @file userid.test.js 
+ * Tests the userid() request function, which returns an Instagram user id given an a valid, existing username.
+ *
+ * @author Mohsin Haider
+ */
+
+const { userid } = require('../src/requests/userid');
+
+test('Should retrieve id of an existing Instagram user', async () => {
+    const instagramUserId = await userid('instagram');
+    expect(instagramUserId).toBe('25025320');
+});
