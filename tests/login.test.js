efe7b4114c57bec59338e76089772f93856bdef0
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-29 21:52:26 -0500

    Wrote test for login() functionality

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,3 +1,10 @@
+/**
+ * @file login.test.js 
+ * Tests the login() request function, which logs in the server as an Instagram user and sends a 200 OK response.
+ *
+ * @author Mohsin Haider
+ */
+
 const app = require('../src/app');
 const supertest = require('supertest');
 
