15e03b64a7fb27dc18e1c6174ef9b4bb10e1a70d
mohsinhaider <mohsin@mohsinhaider.com>
2019-09-30 22:38:58 -0500

    Implement route to get followers

--- src/routes/nonfollower.routes.js
+++ src/routes/nonfollower.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const { followers } = require('../requests/followers');
 
 const router = express.Router();
@@ -7,7 +6,7 @@ const router = express.Router();
 /**
  * Calculates the set of your Instagram followers 
  * that do not follow you back ('nonfollowers').
- * @name POST/api/nonfollowers
+ * @name POST/api/nonfollower
 */
 router.post('/', async (req, res) => {
     const targetInstagramUsername = req.body.username;
@@ -19,5 +18,27 @@ router.post('/', async (req, res) => {
         res.status(500).send(error);
     }
 });
+
+/**
+ * Retrieves your Instagram followers.
+ * @name GET/api/nonfollower/follower
+*/
+router.get('/follower', async (req, res) => {
+    const targetInstagramUsername = req.query.username;
+
+    let followersList;
+    try {
+        followersList = await followers(targetInstagramUsername, process.env.SERVER_CSRF_TOKEN_VALUE, process.env.SERVER_SESSION_ID_VALUE);
+    } catch (error) {
+        if (error === 'Instagram followers service could not successfully respond.') {
+            res.status(500).send(error);
+        }
+        else {
+            res.status(400).send(error);
+        }
+    }
+
+    res.status(200).send(followersList);
+})
 
 module.exports = router;
