77154237d799425b9bb81ebef679785e223f71fe
mohsinhaider <mohsin@mohsinhaider.com>
2019-10-06 23:59:24 -0500

    Implemented pattern to synchronously call dependent requests

--- src/requests/followers.js
+++ src/requests/followers.js
@@ -2,10 +2,14 @@ const request = require('request');
 const { metadata } = require('./metadata');
 const { followersHeaders } = require('../constants/headers');
 const { FOLLOWERS_REQUEST_ERROR } = require('../constants/responses');
+const fs = require('fs');
+
+const { requestWrapper } = require('./requestwrapper');
 
 module.exports = {
     followers: (targetInstagramUsername, csrfToken, sessionId) => {
         return new Promise(async (resolve, reject) => {
+            // Store metadata object to get user properties needed for this request
             let instagramUserMetadata;
             try {
                 instagramUserMetadata = await metadata(targetInstagramUsername);
@@ -14,42 +18,78 @@ module.exports = {
                 return reject(error);
             }
 
+            // Set the follower request batch size
+            const followerBatchCount = 20;
+
             // Setup query string and payload for Instagram followers request
             const followersGraphqlQueryHash = 'c76146de99bb02f6415203be841dd25a';
-            const followersVariables = {
+            let followersVariables = {
                 id: instagramUserMetadata.id,
                 include_reel: true,
                 fetch_mutual: true,
-                first: 24
+                first: followerBatchCount
+                // after: "END_CURSOR_HERE"
             }
-            const followersRequestUrl = `https://www.instagram.com/graphql/query/?query_hash=${followersGraphqlQueryHash}&variables=${encodeURIComponent(JSON.stringify(followersVariables))}`;
+            let followersRequestUrl = `https://www.instagram.com/graphql/query/?query_hash=${followersGraphqlQueryHash}&variables=${encodeURIComponent(JSON.stringify(followersVariables))}`;
+            
+            // Add authentication tokens to request header object
             const sessionIdCookieKeyValuePair = 'sessionid=' + sessionId + ';';
-
             followersHeaders['Cookie'] = sessionIdCookieKeyValuePair;
             followersHeaders['X-CSRFToken'] = csrfToken;
 
-            // Instagram user ID is guarentee to be stored in `instagramUserId` before this request is sent
-            request.get({
-                headers: followersHeaders,
-                gzip: true,
-                url: followersRequestUrl
-            }, (error, response) => {
-                // Breaking Instagram followers endpoint changes
-                if (response.statusCode != 200 || error) {
-                    return reject(FOLLOWERS_REQUEST_ERROR);
+            // Set number of followers to grab in total and calculate total number of needed requests
+            const totalFollowerCount = instagramUserMetadata.edge_followed_by.count;
+            const batchRequestCount = Math.ceil(totalFollowerCount / followerBatchCount);
+
+            let followers = [];
+            let queryEndCursor = '';
+
+            // Instagram user ID is guarenteed to be stored in `instagramUserId` before this request is sent
+            for (let i = 0; i < batchRequestCount; i++) {
+                if (queryEndCursor) {
+                    followersVariables['after'] = queryEndCursor;
+                    followersRequestUrl = `https://www.instagram.com/graphql/query/?query_hash=${followersGraphqlQueryHash}&variables=${encodeURIComponent(JSON.stringify(followersVariables))}`
                 }
                 
-                // Should be in try-catch if properties change
-                const responseObject = JSON.parse(response.body);
-                const followers = responseObject['data']['user']['edge_followed_by']['edges'];
-
-                // Expired sessionid or csrftoken; authentication issue in Cookie header; user has 0 followers
-                if (followers.length == 0) { // TODO: Fix 0 followers case
-                    return reject(FOLLOWERS_REQUEST_ERROR);
+                let requestTask = () => {
+                    return new Promise((resolve, reject) => {
+                        request.get({
+                            headers: followersHeaders,
+                            gzip: true,
+                            url: followersRequestUrl
+                        }, (error, response) => {
+                            // Breaking Instagram followers endpoint changes
+                            if (response.statusCode != 200 || error) {
+                                return reject(FOLLOWERS_REQUEST_ERROR);
+                            }
+                            
+                            // TODO: Should be in try-catch if properties change
+                            const responseObject = JSON.parse(response.body);
+                            const followersBatch = responseObject['data']['user']['edge_followed_by']['edges'];
+            
+                            // Expired sessionid or csrftoken; authentication issue in Cookie header; user has 0 followers
+                            if (followersBatch.length == 0) { // TODO: Fix 0 followers case
+                                return reject(FOLLOWERS_REQUEST_ERROR);
+                            }
+    
+                            // Update end cursor, to be used in next immediate request's query string to point cursor to next batch
+                            queryEndCursor = responseObject['data']['user']['edge_followed_by']['page_info']['end_cursor'];
+                            
+                            // Push current batch of followers onto existing followers array; do not create new array
+                            followers.push.apply(followers, followersBatch);
+                            resolve();
+                        });
+                    });
                 }
 
-                return resolve(followers);
-            });
+                await requestWrapper(requestTask);
+                console.log(i);
+                console.log('----');
+
+                // TODO: if i+1 will equal batchRequestCount AND followers.length != totalFollowerCount, making 1 MORE request to get the last set of followers.
+                // Issue with Instagram API, sometimes it will return less than the follower batch size. Alternative could be to sleep, try that first? (might make for worse user experience).
+            }
+            fs.writeFileSync('out.txt', JSON.stringify(followers));
         })
     }
 }
