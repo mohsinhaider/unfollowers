81074dc2cce5cea6138aacb100b97277f8d8504d
mohsinhaider <mohsin@mohsinhaider.com>
2019-10-13 23:54:01 -0500

    Setup handlebars

---
+++ src/routes/api.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { login } = require('../requests/login');
+
+const router = express.Router();
+
+/**
+ * Logs in the API server to Instagram
+ * @name GET/api/login
+*/
+router.get('/login', async (req, res) => {
+    try {
+        const response = await login();
+        res.status(200).send('Login was successful');
+    }
+    catch (error) {
+        res.render('index/404')
+    }
+});
+
+/**
+ * Retrieves the CSRF token generated at login time.
+ * @name GET/api/token
+*/
+router.get('/token', (req, res) => {
+    res.send(process.env.SERVER_CSRF_TOKEN_VALUE);
+});
+
+module.exports = router;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,28 +1,9 @@
 const express = require('express');
-const { login } = require('../requests/login');
 
 const router = express.Router();
 
-/**
- * Logs in the API server to Instagram
- * @name GET/api/login
-*/
-router.get('/login', async (req, res) => {
-    try {
-        const response = await login();
-        res.status(200).send('Login was successful');
-    }
-    catch (error) {
-        res.render('index/404')
-    }
-});
-
-/**
- * Retrieves the CSRF token generated at login time.
- * @name GET/api/token
-*/
-router.get('/token', (req, res) => {
-    res.send(process.env.SERVER_CSRF_TOKEN_VALUE);
+router.get('/', (req, res) => {
+    res.render('index/home');
 });
 
 module.exports = router;
