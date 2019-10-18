ff46e3d04b34e8a2975e7e419bc5d1e69d8eab45
mohsinhaider <mohsin@mohsinhaider.com>
2019-10-17 23:16:32 -0500

    setup click handler

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,27 @@
-axios({
-    method: 'get',
-    url: '/api/nonfollower/follower',
-    params: {
-        username: 'roxy.tillerson'
+const submitButton = document.querySelector('#submit-button');
+const usernameInput = document.querySelector('#username-input');
+
+console.log(usernameInput)
+
+submitButton.addEventListener('click', () => {
+    if (usernameInput.value) {
+        console.log('hello');
     }
-}).then((response) => {
-    console.log(response.data);
-}).catch((error) => {
-    console.log(error);
 });
+
+
+let cleanHandle = () => {
+    
+}
+
+// axios({
+//     method: 'post',
+//     url: '/api/nonfollower',
+//     data: {
+//         username: 'mohsinwho'
+//     }
+// }).then((response) => {
+//     console.log(response.data);
+// }).catch((error) => {
+//     console.log(error);
+// });
