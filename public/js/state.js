ce93ee501d2ff57406cc6cdacbcb10f9edf02c66
mohsinhaider <mohsin@mohsinhaider.com>
2019-10-19 17:49:00 -0500

    Rewrite logic for state update validation

--- public/js/state.js
+++ public/js/state.js
@@ -1,13 +1,22 @@
 class State {
     static update(executor, key, value) {
         try {
-            executor();
+            if (this.shouldExecute(key, value)) {
+                executor();
+                State.events[key] = value;
+            }
         }   
         catch (error) {
             console.log('Executor could not run; state was not updated.');
         }
+    }
 
-        State.events[key] = value;
+    static shouldExecute(key, value) {
+        let keyExists = (key in State.events);
+        if ((!keyExists && !value) || (keyExists && State.events[key] === value)) {
+            return false;
+        }
+        return true;
     }
 }
 
