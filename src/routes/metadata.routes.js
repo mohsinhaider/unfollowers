f0173159d4ab97a53cd8fef4c6ea5f121112465f
mohsinhaider <mohsin@mohsinhaider.com>
2019-10-20 16:45:17 -0500

    Implement metadata GET endpoint

--- src/routes/metadata.routes.js
+++ src/routes/metadata.routes.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const { metadata } = require('../requests/metadata') 
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.sendStatus(200);
+    const targetInstagramUsername = req.query.username;
+    let rawMetadata, metadataPayload;
+    rawMetadata = metadataPayload = null;
+
+    try {
+        rawMetadata = await metadata(targetInstagramUsername);
+        metadataPayload = {
+            id: rawMetadata.id,
+            username: rawMetadata.user,
+            fullName: rawMetadata.full_name,
+            followerCount: rawMetadata.edge_followed_by.count,
+            followingCount: rawMetadata.edge_follow.count
+        }
+    }
+    catch (error) {
+        // Move non-follower USERID catch switch here
+        res.sendStatus(500);
+    }
+
+    res.status(200).send(metadataPayload);
 });
 
 module.exports = router;
