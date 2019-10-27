bc3539558aca3ef7758015f683d1db409eea1530
mohsinhaider <mohsin@mohsinhaider.com>
2019-10-26 23:11:23 -0500

    Display and format full name with handle

--- public/js/home.js
+++ public/js/home.js
@@ -285,7 +285,15 @@ let renderNonfollowersTable = (nonfollowers) => {
         profilePicCell.innerHTML = profilePic.outerHTML;
 
         let handleRow = nonfollowerMetadataRow.insertCell(1);
-        handleRow.innerHTML = nonfollower.username;
+        handleRow.id = 'nonfollower-text-cell';
+        let handleParagraph = document.createElement('p');
+        handleParagraph.id = 'nonfollower-handle';
+        handleParagraph.innerText = nonfollower.username;
+        let fullNameParagraph = document.createElement('p');
+        fullNameParagraph.id = 'nonfollower-fullname';
+        fullNameParagraph.innerText = nonfollower.fullName;
+        handleRow.appendChild(handleParagraph);
+        handleRow.appendChild(fullNameParagraph);
 
         rowCount++;
     });
