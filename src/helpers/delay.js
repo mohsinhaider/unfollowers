29c746a3aac8c42a63d2239365da9e250d902a85
mohsinhaider <mohsin@mohsinhaider.com>
2019-11-04 22:05:00 -0600

    Remove random-based delay time calculations

--- src/helpers/delay.js
+++ src/helpers/delay.js
@@ -1,13 +1,11 @@
 module.exports = {
     delay: (userCount) => {
         let delayMs = 0;
-        if (userCount >= 3500) {
-            const lambd = 0.7;
-            const expRandom = (-Math.log(1.0 - Math.random()) / lambd);
-            delayMs = Math.min(expRandom * 1000, 5000);
-        } else {
+
+        if (userCount <= 4000) {
             delayMs = 39;
         }
+ 
         return delayMs;
     }
 }
