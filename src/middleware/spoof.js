0d514580b4f086fbfe82315480404a74da7a49fa
mohsinhaider <mohsin@mohsinhaider.com>
2019-11-04 22:04:35 -0600

    Add return to stop further execution

--- src/middleware/spoof.js
+++ src/middleware/spoof.js
@@ -3,7 +3,7 @@ module.exports = {
         const referer = req.headers.referer;
         if ((process.env.ENVIRONMENT_NAME === 'production' && referer.includes('straws')) 
             || process.env.ENVIRONMENT_NAME === 'development') {
-            next();
+            return next();
         }
         return res.status(500).send({ error: '500 - Request could not be served' });
     }
